e3a9457d51169d4ed7734239acdfa490f20a9068
localcc <work@localcc.cc>
2022-12-29 16:39:12 +0100

    fix formatting

--- ExampleMod/dllmain.cpp
+++ ExampleMod/dllmain.cpp
@@ -1,16 +1,13 @@
-#include <windows.h>
 #include "ExampleMod.h"
-//Mod* CoreMod;
+#include <windows.h>
+// Mod* CoreMod;
 
 void CreateMod()
 {
     auto CoreMod = new ExampleMod();
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-    DWORD  ul_reason_for_call,
-    LPVOID lpReserved
-)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
